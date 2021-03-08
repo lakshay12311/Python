109c423e2de0870d64fa43ff560da348c15e039d
lakshay12311 <72252002+lakshay12311@users.noreply.github.com>
2021-03-08 11:57:12 +0530

    Add files via upload

---
+++ Practice questions.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Q1 WAP to check if 10 is greater than 2 or not
+
+
+# In[1]:
+
+
+a = 2
+b = 10
+
+if a>b: 
+    print("2 is greater")
+else: 
+    print("10 is greater")
+
+
+# In[ ]:
+
+
+Q2 WAP to print variable x, y, z having int, string, boolean values
+
+
+# In[5]:
+
+
+x = int(5)
+y = str("test")
+z = bool(10>11)
+
+print(x)
+print(y)
+print(z)
+
+
+# In[ ]:
+
+
+Q3 Comment the questions 1 & 2
+
+
+# In[7]:
+
+
+#a = 2
+#b = 10
+
+#if a>b: 
+    #print("2 is greater")
+#else: 
+    #print("10 is greater")
+    
+    
+    
+    
+"""a = 2
+b = 10
+
+if a>b: 
+    print("2 is greater")
+else: 
+    print("10 is greater")"""
+
+
+# In[ ]:
+
+
+Q4 Write a three line comment using multi line comment
+
+
+# In[ ]:
+
+
+"""
+adcuygcuyegceucygewuwevce
+"""
+
+Q5 Write the datatypes of the variables in question number 2
+# In[8]:
+
+
+x = int(5)
+y = str("test")
+z = bool(10>11)
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# In[ ]:
+
+
+Q6 Print multiple variables
+
+
+# In[9]:
+
+
+x = int(5)
+y = str("test")
+z = bool(10>11)
+
+print(x, y, z)
+
+
+# In[ ]:
+
+
+Q7 Print the datatype for "hello"
+
+
+# In[10]:
+
+
+x = ("hello")
+
+print(type(x))
+
